bbd951630a4eb88c57e0422f0db214ec7de9685e
Jeff Herrera <jeffreyherrera@Jeffreys-MacBook-Pro.local>
2016-11-03 15:43:50 -0400

    Initial Commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
+$(window).on('load', function() {
+    $('.loader').addClass('load');
+    if ($('.work-content').length) {
+        $('body').addClass('work-body');
+        $('.work-content').addClass('work-load');
+        $('.work-close').addClass('work-close-load');
+        $(".work-close, .thanks-link").click(function(){
+            $('.work-content, .work-close').addClass('unload');
+        });
+        $('.web-project .down-button').click(function(){
+            $('.browser img').addClass('web-scroll');
+        });
+    }
+ });
+
 $(document).ready(function() {
-    $("body").addClass('load');
     $("nav [href]").each(function() {
     if (this.href == window.location.href) {
         $(this).addClass("active");
         }
     });
-    $("nav a").click(function(){
-        $('body').addClass('unload')
+    $("nav a, .work-item").click(function(){
+        $('body').addClass('unload');
+    });
+    $('.mobile-nav-button').click(
+        function(){
+            $(this).toggleClass('mobile-nav-open');
+            $('.mobile-nav').toggleClass('mobile-nav-active');
     });
 });
